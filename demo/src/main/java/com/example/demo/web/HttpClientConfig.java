a2692140124a9319bf08b9c63e6b29eefa59a70f
Nataliia-Kim <kimnataliiait@gmail.com>
2023-08-19 13:12:21 +0300

    correct test

---
+++ demo/src/main/java/com/example/demo/web/HttpClientConfig.java
@@ -0,0 +1,11 @@
+package com.example.demo.web;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+public class HttpClientConfig {
+    @Bean
+    public CloseableHttpClient httpClient() {
+        return HttpClients.createDefault();
+    }
+}
